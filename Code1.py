9ab0733f9a5866cd9c0ab60a041d620a87503193
rohan-databricks <rohan-databricks>
2022-01-28 04:48:29 +0000

    release 28

--- Code1.py
+++ Code1.py
@@ -14,6 +14,7 @@ def generate_data1(n=1000, name='my_cool_data'):
 #removed comments from top and added here
 #testing at 11.35
 #testing at 11.45
+#testing at 11.50
 def upper_columns(df: DataFrame, cols: list) -> DataFrame:
     new_cols = []
     for field in df.schema.fields:
